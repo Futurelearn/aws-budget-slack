ea5e4c8a2060777f69c175d49872d9849b460fd4
Laura Martin <surminus@gmail.com>
2019-07-30 19:27:24 +0100

    Add Lambda function to post AWS budget in Slack
    It would be, er, nice? I guess? If every Monday we got a weekly update
    on how much we're spending in AWS in Slack so we can see if we're
    rapidly spending way too much.

---
+++ README.md
@@ -0,0 +1,6 @@
+# aws-budget-slack
+
+Lambda function to keep us updated with how much we're spending in AWS.
+
+Idea pinched from [AWS-budget-to-slack](https://github.com/richstokes/AWS-budget-to-slack)
+and rewritten in Ruby with added emoji.
---
+++ lambda_function.rb
@@ -0,0 +1,73 @@
+require 'aws-sdk-budgets'
+require 'aws-sdk-core'
+require 'slack-ruby-client'
+
+def call(event: nil, context: nil)
+  if slack_api_token.nil?
+    abort("Must set SLACK_API_TOKEN environment variable!")
+  end
+
+  send_slack_message
+end
+
+private
+
+def budget_client
+  Aws::Budgets::Client.new.describe_budget(
+    account_id: account_id,
+    budget_name: budget_name
+  ).budget
+end
+
+def actual_spend
+  budget_client.calculated_spend.actual_spend.amount.to_i.ceil
+end
+
+def forecast_spend
+  budget_client.calculated_spend.forecasted_spend.amount.to_i.ceil
+end
+
+def set_budget
+  budget_client.budget_limit.amount.to_i.ceil
+end
+
+def slack_message_content
+  <<~TEXT
+    :moneybag: *AWS Budget Report* (for #{Date.today.strftime("%B")}) :money_with_wings:
+
+    The monthly budget is: *$#{set_budget}* :face_with_monocle:
+
+    The actual spend so far this month is: *$#{actual_spend}* :dollar:
+
+    The forecasted spend is: *$#{forecast_spend}* :sweat_smile:
+
+    #{":sadwich:" if actual_spend > set_budget}
+    #{":monopoly:" if forecast_spend < set_budget}
+  TEXT
+end
+
+def send_slack_message
+  slack = Slack::Web::Client.new(token: slack_api_token)
+
+  slack.chat_postMessage(channel: slack_channel, text: slack_message_content, as_user: true)
+end
+
+def slack_api_token
+  ENV['SLACK_API_TOKEN']
+end
+
+def slack_channel
+  ENV['SLACK_CHANNEL'] || '#ops'
+end
+
+def account_id
+  Aws::STS::Client.new.get_caller_identity.account
+end
+
+def budget_name
+  ENV['BUDGET_NAME'] || 'Monthly Total AWS Budget'
+end
+
+def client
+  Aws::Budgets::Client.new(account_id: account_id, )
+end
